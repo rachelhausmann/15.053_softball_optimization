ede654444f5a52fef91c4a6a5061998568d03b99
Rachel Hausmann <rach@mit.edu>
2017-04-22 19:11:03 -0400

    Calculates expected score for given order
    In this state if it is run, it runs batting orders that are all one person
    to test if the results make sense. They do

--- softball_final_proj.py
+++ softball_final_proj.py
@@ -11,25 +11,48 @@ outcome_words = ["walk", "strikeout", "single", "double", "triple", "homerun", "
 # state defined as (on 1st, on 2nd, on 3rd, inning, outs, score, batter index)
 
 order = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+order = [0]*9
 start = (False, False, False, 1, 0, 0, 0)
 states = set()
 states.add(start)
 state_prob = {start: 1}
-score_prob = {}
+exp_score = 0
 
-state = states.pop()
-for i in range(len(outcomes)):
-    res = outcomes[i](state)
-    states.add(res)
-    if res not in state_prob:
-        state_prob[res] = 0
-    state_prob[res] += state_prob[state]*batters[order[state[6]]][outcome_words[i]]
-state_prob[state] = 0
+# state = states.pop()
+# for i in range(len(outcomes)):
+#     res = outcomes[i](state)
+#     states.add(res)
+#     if res not in state_prob:
+#         state_prob[res] = 0
+#     state_prob[res] += state_prob[state]*batters[order[state[6]]][outcome_words[i]]
+# state_prob[state] = 0
+#
+# pprint.pprint(states)
+# pprint.pprint(state_prob)
+for batter in range(10):
+    order = [batter]*9
+    states = set()
+    states.add(start)
+    state_prob = {start: 1}
+    exp_score = 0
+    while len(states) > 0:
+        state = states.pop()
+        for i in range(len(outcomes)):
+            res = outcomes[i](state)
+            res_prob = state_prob[state]*batters[order[state[6]]][outcome_words[i]]
+            # game finished
+            if res[0] == "end":
+                exp_score += res[1]*res_prob
+            # game continuing
+            else:
+                states.add(res)
+                if res not in state_prob:
+                    state_prob[res] = 0
+                state_prob[res] += res_prob
+        state_prob[state] = 0
 
-pprint.pprint(states)
-pprint.pprint(state_prob)
+    print batters[batter]["name"], ":", exp_score
 
-# while len(states) > 0:
-#     state = states.pop()
+
 
 
